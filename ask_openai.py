ec8a01cf969651e2428e4531d8c9ff90c599e178
Evan Kiser <kiser.evan1@gmail.com>
2023-04-27 16:18:05 -0700

    Thats a full box right there

--- ask_openai.py
+++ ask_openai.py
@@ -3,8 +3,6 @@ import os
 import openai
 import requests
 from datetime import datetime
-import time
-import sys
 
 load_dotenv()
 # Set up OpenAI API credentials
@@ -20,23 +18,14 @@ def ask_openai(prompt, role, model_choice='gpt-3.5-turbo', tokens=5000, temp=0.8
             You are a professional fiction writer who is a best-selling author. 
             You use all of the rhetorical devices you know to write a compelling book.
         """
+
     now = datetime.now()
-    num_retries = 0
-    max_retry = 3
-    while num_retries < max_retry:
-        try:
-            response = openai.ChatCompletion.create(
-                model=model_choice,
-                messages=[{"role": "system", "content": role_content}, {"role": "user", "content": prompt}],
-                temperature=temp,
-                max_tokens=tokens,
-            )
-        except openai.error.Timeout:
-            print("Timeout occurred. Retrying in {} seconds...".format(2 ** num_retries))
-            num_retries += 1
-            time.sleep(2 ** num_retries)
-    if num_retries > max_retry:
-        print("Max retries reached. Aborting...")
-        sys.exit(1)
+    print(now)
+    response = openai.ChatCompletion.create(
+        model=model_choice,
+        messages=[{"role": "system", "content": role_content}, {"role": "user", "content": prompt}],
+        temperature=temp,
+        max_tokens=tokens,
+    )
     print(f"OpenAI Response Time: {datetime.now() - now} ms")
     return response
